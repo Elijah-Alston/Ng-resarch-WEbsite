104b32b558b705c7216458394acfdc324fc39d73
flameking1000 <elijahalstonjr@gmail.com>
2024-10-15 12:17:49 -0400

    add some small tips

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          NG RESARCH WEBSITE
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Elijah-Alston/Ng-resarch-WEbsite"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GITHUB
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
